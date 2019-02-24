53d2ff03684f50c35f961a915bbe68f3a6b93ed0
boy-hack <34109680@qq.com>
2019-02-24 21:37:24 +0800

    title 插件修复

--- plugins/webtitle.py
+++ plugins/webtitle.py
@@ -16,6 +16,6 @@ def poc(target):
     '''
     html = collector.get_domain_info(target, "body")
     if html:
-        m = re.search('<title>(.*)?<\/title>', html, re.IGNORECASE)
+        m = re.search('<title>(.*)?<\/title>', html, re.S)
         if m:
-            collector.add_domain_info(target, {"title": m.group(1)})
+            collector.add_domain_info(target, {"title": m.group(1).strip()})
