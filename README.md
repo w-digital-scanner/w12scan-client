8768290bd51a05605086f64fbf639dcb2d217041
boy-hack <34109680@qq.com>
2019-02-25 14:41:20 +0800

    加入目录浏览插件

--- README.md
+++ README.md
@@ -1,35 +1,18 @@
 # w12scan-client
 网络资产搜索发现引擎,大规模资产识别，毕业设计，w12scan 客户端程序
 
-## 扫描器架构
+## 配置说明
+- 这是w12scan的扫描端，基于python3编写，在进行扫描之前，需要安装`nmap`和`masscan`，并将它们设置为环境变量，当然，如果你是使用docker编译安装的就不需要此步骤了。
+- `config.py`中定义了扫描的相关配置，你可以设置masscan扫描的`rate`，是否扫描全端口(默认扫描部分端口)，线程数以及节点名称等等...
 
-![jiagou](doc/w12scan.jpg)
-
-## 插件规范
-参见 [https://github.com/boy-hack/airbug](https://github.com/boy-hack/airbug)队列。
-
-## Todo List
-- [x] 插件调用采用线程方式在多线程中扫描
-- [x] 插件的规范规则
-- [ ] 完成WEB类插件
-    - [x] BODY类
-        - WAF识别
-        - wappalyer分析
-        - title
-    - [x] 敏感文件扫描 
-    - [x] 优化备份文件扫描
-    - [x] WEB指纹分析
-        - [x] 可以远程调用AIRBUG https://github.com/boy-hack/airbug
-    - [ ] 后台目录扫描(自动识别语言后扫描)
-- [ ] 完成IP类插件
-- [x] ip地理位置查询服务使用geo数据库
-
-## For mac
-
-- brew install masscan
-- brew install nmap
-
-sudo运行程序
+## docker编译
+- 本仓库所有代码提交都会自动编译到dockerhub上，所以你无需下载这里的仓库。你只需要按照(https://github.com/boy-hack/w12scan)[https://github.com/boy-hack/w12scan]的要求运行即可。
+- 除非你想自己修改一些配置可以使用如下命令,在本文件目录下
+```bash
+docker build -t w12scan-client:lastest .
+docker tag w12scan-client:latest boyhack/w12scan-client
+```
+再按照(https://github.com/boy-hack/w12scan)[https://github.com/boy-hack/w12scan]的要求运行即可。
 
 # IP数据库更新
 - 网络上大部分ip接口都有频率限制，所以IP数据使用GEOIP数据，但不保证数据的时效性，可以通过下载最新数据库解决
---
+++ plugins/directory_browse.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/2/25 2:38 PM
+# @Author  : w8ay
+# @File    : directory_browse.py
+# Author:w8ay
+# Name:目录浏览
+
+import HackRequests
+from urllib.parse import urlparse
+from lib.data import collector
+
+
+def poc(arg, **kwargs):
+    URL = arg
+    netloc = urlparse(arg).netloc
+    flag_list = [
+        "<title>index of",
+        "<title>directory listing for",
+        "<title>{} - /".format(netloc)
+    ]
+    hack = HackRequests.hackRequests()
+    url_list = [
+        URL + "/css/", URL + "/js/", URL + "/img/", URL + "/images/", URL + "/upload/", URL + "/inc/"
+    ]
+    for u in url_list:
+        try:
+            hh = hack.http(u)
+        except:
+            continue
+        if hh.status_code == 404:
+            continue
+        for i in flag_list:
+            if i in hh.text():
+                result = {
+                    "name": "web目录浏览",  # 插件名称
+                    "content": "通过此功能可获取web目录程序结构",  # 插件返回内容详情，会造成什么后果。
+                    "url": u,  # 漏洞存在url
+                    "log": hh.log,
+                    "tag": "info_leak"  # 漏洞标签
+                }
+                collector.add_domain_bug(arg, {"directory_browse": repr(result)})
+    return False
