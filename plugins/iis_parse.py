e74b01eadc2256a7f235908f02fe06e380b23d8a
boy-hack <34109680@qq.com>
2019-02-28 18:27:42 +0800

    增强iis解析模块准确性

--- plugins/iis_parse.py
+++ plugins/iis_parse.py
@@ -3,6 +3,8 @@
 # @Time    : 2019/1/23 6:13 PM
 # @Author  : w8ay
 # @File    : iis_parse.py
+import re
+
 import requests
 
 from lib.data import collector
@@ -15,8 +17,9 @@ def poc(arg):
         header[
             "User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
         r = requests.get(url, headers=header, timeout=5, allow_redirects=False)
-        if r.status_code == 200 and "<html" not in r.text and "text/html" in r.headers.get("Content-Type", ""):
-            collector.add_domain_bug(arg, {"iis parse": url})
+        if r.status_code == 200 and "text/html" in r.headers.get("Content-Type", ""):
+            if re.search('<(html|title)', r.text, re.I | re.M | re.S):
+                collector.add_domain_bug(arg, {"iis parse": url})
         else:
             return False
     except Exception:
