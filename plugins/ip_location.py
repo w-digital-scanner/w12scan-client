1ee6152c09c29197206f2aceebb369a6f91c9ebe
boy-hack <34109680@qq.com>
2019-04-21 10:57:25 +0800

    内网IP判断

--- plugins/ip_location.py
+++ plugins/ip_location.py
@@ -4,6 +4,7 @@
 # @Author  : w8ay
 # @File    : ip_location.py
 import os
+from functools import reduce
 
 import requests
 import json
@@ -46,6 +47,27 @@ def geoip(arg):
     return d
 
 
+def ip_into_int(ip):
+    # 先把 192.168.1.13 变成16进制的 c0.a8.01.0d ，再去了“.”后转成10进制的 3232235789 即可。
+    # (((((192 * 256) + 168) * 256) + 1) * 256) + 13
+    return reduce(lambda x, y: (x << 8) + y, map(int, ip.split('.')))
+
+
+def is_internal_ip(ip):
+    ip = ip_into_int(ip)
+    net_a = ip_into_int('10.255.255.255') >> 24
+    net_b = ip_into_int('172.31.255.255') >> 20
+    net_c = ip_into_int('192.168.255.255') >> 16
+    return ip >> 24 == net_a or ip >> 20 == net_b or ip >> 16 == net_c
+
+
 def poc(ip):
+    if is_internal_ip(ip):
+        d = {
+            "country_id": "internal icon-disc",
+            "country": "Internal Ip",
+            "region": ""
+        }
+        return d
     interface = geoip(ip)
     return interface
